ad0dbbd3d07c712aede593744d745c7291b191bf
casper7609 <casper7609@naver.com>
2017-02-23 18:19:16 +0900

    commit

---
+++ serverscript.js
@@ -0,0 +1,81 @@
+handlers.PurchaseCharacter = function (args) {
+    log.info("PlayFabId " + args.PlayFabId);
+    log.info("ClassType " + args.ClassType);
+    log.info("ClassStatus " + args.ClassStatus);
+    var classType = args.ClassType;
+
+    var gemPrice = 0;
+    var allChars = server.GetAllUsersCharacters({
+        "PlayFabId": currentPlayerId
+    });
+    if (allChars.Characters.length < 4) {
+        gemPrice = 0;
+    }
+    else {
+        gemPrice = 400 * Math.pow(2, (allChars.Characters.length - 4));
+    }
+    log.info("gemPrice " + gemPrice);
+
+    var userInv = server.GetUserInventory({
+        "PlayFabId": currentPlayerId
+    });
+    var currentGem = userInv.VirtualCurrency.GP;
+    if (currentGem < gemPrice) {
+        return { "Error": "Insufficient Gem" };
+    }
+    if (gemPrice > 0) {
+        server.SubtractUserVirtualCurrency(
+            {
+                "PlayFabId": currentPlayerId,
+                "VirtualCurrency": "GP",
+                "Amount": gemPrice
+            }
+        );
+    }
+
+    var grantCharResult = server.GrantCharacterToUser({
+        "PlayFabId": currentPlayerId,
+        "CatalogVersion": catalogVersion,
+        "CharacterName": classType,
+        "CharacterType": classType,
+        "ItemId": classType
+    });
+    var characterId = grantCharResult.CharacterId;
+    log.info("characterId " + characterId);
+    var classStatus = JSON.parse(args.ClassStatus);
+    var luck = classStatus["Luck"];
+    delete classStatus["Luck"];
+    server.UpdateCharacterData({
+        "PlayFabId": currentPlayerId,
+        "CharacterId": characterId,
+        "Data": classStatus
+    });
+    server.UpdateCharacterData({
+        "PlayFabId": currentPlayerId,
+        "CharacterId": characterId,
+        "Data": { "Luck": luck, "IsActive": "true" }
+    });
+    var itemId = "";
+    if (classType == "Rogue") {
+        itemId = "Dagger_00";
+    }
+    else if (classType == "Hunter") {
+        itemId = "Bow_00";
+    }
+    else if (classType == "Warrior" || classType == "SpellSword" || classType == "Paladin") {
+        itemId = "TwoHandSword_00";
+    }
+    else if (classType == "Sorcerer" || classType == "Warlock" || classType == "Priest") {
+        itemId = "Staff_00";
+    }
+
+    log.info("itemId " + itemId);
+    var grantItemResult = server.GrantItemsToCharacter({
+        "Annotation": "Char Creation Basic Item",
+        "CatalogVersion": catalogVersion,
+        "PlayFabId": currentPlayerId,
+        "CharacterId": characterId,
+        "ItemIds": [itemId]
+    });
+    log.info("grantItemResult " + JSON.stringify(grantItemResult));
+};
